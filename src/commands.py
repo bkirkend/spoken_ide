92a15ffb163b86505a710a9979b36863aa73f0fd
Braden Kirkendalll <bdkirkendall@yahoo.com>
2025-02-02 16:03:35 -0500

    clear gpt history fxn

--- src/commands.py
+++ src/commands.py
@@ -1,6 +1,6 @@
 #command parser based on spoken input
 
-from gpt import gpt
+from gpt import *
 
 def parse_command(msg):
     pass
@@ -13,6 +13,9 @@ def handle_line(msg, line_num):
     suffix = f"only change line {line_num}"
     print(output := gpt(msg + suffix))
     return output
+
+def handle_clear_history(msg):
+    clear_discourse()
 
 def handle_run(msg):
     pass
@@ -40,6 +43,7 @@ command_handler = {
     "prompt" : handle_prompt,
     "line" : handle_line,
     "run" : handle_run,
+    "clear history" : handle_clear_history,
     "up" :  handle_up,
     "down" :  handle_down,
     "right" :  handle_right,
